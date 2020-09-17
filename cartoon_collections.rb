e6beb38421fbd5c823397ab4c1308e9d2ff84e9f
Zachary Darneille <zakdarn@gmail.com>
2020-09-17 00:30:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)# code an argument here
   end
 
 def find_the_cheese(mouse)# code an argument here
-    mouse.detect{|i|odd?}# the array below is here to help
+    mouse.detect{|i|odd?}
+end# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
 end
